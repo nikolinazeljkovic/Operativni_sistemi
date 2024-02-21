6c5a1d152406614a40553d6c80c37a08a088b066
Nikolina <102320119+nikolinazeljkovic@users.noreply.github.com>
2024-02-21 20:04:32 +0100

    Update Register.java

--- asembler/Register.java
+++ asembler/Register.java
@@ -1,1 +1,15 @@
+package asembler;
 
+public class Register 
+{
+	public final String name;
+    public final String address;
+    public int value;
+     
+    
+    public Register(String name, String address, int value) {
+ 	    this.name=name;
+ 	    this.address=address;
+ 	    this.value=value;
+    }
+}
